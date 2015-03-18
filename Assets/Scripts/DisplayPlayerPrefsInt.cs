7c53fe1b24c3d485cef02fcd2faffc800598ec3b
Valentin Baltadzhiev <valo.balt@gmail.com>
2015-03-18 09:51:51 +0000

    Test Comming

--- Assets/Scripts/DisplayPlayerPrefsInt.cs
+++ Assets/Scripts/DisplayPlayerPrefsInt.cs
@@ -15,6 +15,7 @@ public class DisplayPlayerPrefsInt : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         this.GetComponent<Text>().text = PlayerPrefs.GetInt(Key).ToString();
+        Debug.Log("PlayerPrefsInt");
 	}
 	
 	// Update is called once per frame
